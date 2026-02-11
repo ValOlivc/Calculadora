90dc1bcc6ddd64444b54b7718319b00180cf574b
val6-uwu <volivares542@gmail.com>
2026-02-11 13:02:44 -0500

    Calculadora

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>calculadora</title>
+</head>
+<body>
+    <h2>Calculadora</h2>
+    <form method="post" action="<?php $_SERVER['PHP_SELF'] ?>">
+        <label for=""></label>
+    </form>
+</body>
+</html>
